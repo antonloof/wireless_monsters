29d65aa8ae2e94dddf4a307a90b5794f9112eaef
Anton Lööf <anton@loofarna.se>
2021-11-07 21:39:18 +0100

    Explore barker code

--- transmitter.m
+++ transmitter.m
@@ -3,10 +3,10 @@ function [txdata, t_up] = transmitter(symbs, oversampl, fs, rrcos_filt)
 %   Detailed explanation goes here
     symbs_i_pulse_shaped = upfirdn(real(symbs), rrcos_filt, oversampl, 1);
     symbs_q_pulse_shaped = upfirdn(imag(symbs), rrcos_filt, oversampl, 1);
-    t = linspace(0, length(symbs), length(symbs_i_pulse_shaped));
     symbs_i_pulse_shaped_up = resample(symbs_i_pulse_shaped, fs, 1);
     symbs_q_pulse_shaped_up = resample(symbs_q_pulse_shaped, fs, 1);
-    t_up = linspace(min(t), max(t), length(symbs_q_pulse_shaped_up));
+    
+    t_up = linspace(0, length(symbs), length(symbs_q_pulse_shaped_up));
     lo_in = sin(2*pi*fs*t_up);
     lo_out = cos(2*pi*fs*t_up);
 
