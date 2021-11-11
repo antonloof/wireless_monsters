5ebf0db0c43f0dd3576b1dd0167fdb44e602c22c
Anton Lööf <anton@loofarna.se>
2021-11-11 09:28:53 +0100

    Add barker code id and phase/gain corr

--- dsp test.m
+++ dsp test.m
@@ -1,65 +1,66 @@
 clc, close all, clear all
 
-flo = 20;
-oversampl = 6;
-rrcos_filt_len = 6;
+fs = 5;
+flo = 10;
+oversampl = 8;
+rrcos_filt_len = 8;
 rrcos_filt_beta = 0.25;
 rrcos_filt = rcosdesign(rrcos_filt_beta, rrcos_filt_len, oversampl);
 
 bits_per_symb = 4;
 bits_per_item = 8;
-data = randi(2^bits_per_item - 1, 1, 100);
-data = 'kalle anka';
+data = randi(2^bits_per_item, 1, 100) - 1;
 barker_code13 = [+1 +1 +1 +1 +1 -1 -1 +1 +1 -1 +1 -1 +1];
 barker_symbs = (1+i)*barker_code13;
+barker_symbs_up = upfirdn(barker_symbs, rrcos_filt, oversampl);
 
 bin_symbs = symbolify(data, bits_per_item, bits_per_symb);
 
 points_per_axis = 2^(bits_per_symb/2);
-qam16_matrix = (-1:2/(points_per_axis-1):1) + (i*(-1:2/(points_per_axis-1):1)');
-qam16_lookup = reshape(qam16_matrix.',1,[]);
+qam_matrix = (-1:2/(points_per_axis-1):1) + (i*(-1:2/(points_per_axis-1):1)');
+qam_lookup = qam_matrix(:)';
 
-symbs = qam16_lookup(bin_symbs+1);
-packet_header = [barker_symbs zeros(1, oversampl-1)];
-packet_header = [];
+symbs = qam_lookup(bin_symbs+1);
+packet_header = [barker_symbs zeros(1, oversampl - 1)];
 packet = [packet_header symbs];
-[data_to_send, t_tx] = transmitter(packet, oversampl, flo, rrcos_filt);
+[data_to_send, t_tx] = transmitter(packet, oversampl, flo, rrcos_filt, fs);
 
-% hw simulator
-some_random_offset = zeros(1, (randi(100) + 10) * flo);
-some_random_offset = zeros(1, 0*oversampl*flo);
-data_recieved = awgn([some_random_offset data_to_send], 20); % do I even need HW?
-data_recieved = [some_random_offset data_to_send];
+% the channel is HERE
+% do I even need HW?
+the_num = randi(100) + 10;
+some_random_offset = zeros(1, the_num * flo * fs);
+data_recieved = awgn([some_random_offset data_to_send], 30);
+df = 1+1e-5;
+some_phase = 2*pi*(rand()-0.5);
 
-df = 1;
-some_phase = 0;
-t_rx = linspace(1, length(data_recieved)/flo/oversampl, length(data_recieved));
-lo_i = sin(2*pi*flo*df*t_rx+some_phase);
-lo_q = cos(2*pi*flo*df*t_rx+some_phase);
-plot(t_rx, lo_i);
-b = fir1(2*flo,2/flo, 'low');
+t_rx = 0:1/fs:((length(data_recieved)-1)/fs);
+
+lo_i = sin(df*t_rx+some_phase);
+lo_q = cos(df*t_rx+some_phase);
+
+b = fir1(2*flo*fs,2/fs/flo);
 rx_with_lo = data_recieved .* lo_i + 1j*data_recieved .* lo_q;
-rx_high_sample = upfirdn(b, rx_with_lo, 1, flo);
-plot(linspace(1, length(symbs), length(rx_high_sample)), real(rx_high_sample))
-barker_upsampled = upfirdn(barker_symbs, rrcos_filt, oversampl);
-packet_upsampled = upfirdn(packet, rrcos_filt, oversampl);
 
-figure
-hold on
-plot(abs(xcorr(barker_upsampled, packet_upsampled)))
-plot(real(packet_upsampled))
-plot(real(barker_upsampled), 'o')
+rx_high_sample = upfirdn(b, rx_with_lo, 1, flo*fs);
+rx_high_sample_shift = rx_high_sample(2:end-1);
 
-%figure 
-%hold on
-%plot(linspace(0,length(packet_upsampled),length(rx_with_lo)), real(rx_with_lo))
-%plot(real(rx_high_sample))
+abs_corr = abs(xcorr(rx_high_sample_shift, barker_symbs_up));
+[v, corr_max_index] = max(abs_corr);
+packet_offset = corr_max_index-length(rx_high_sample_shift);
 
-rx = upfirdn(rrcos_filt, rx_high_sample, 1, oversampl);
+rx_high_sample_shift_packet = rx_high_sample_shift((packet_offset+1):end);
+
+rx = upfirdn(rrcos_filt, rx_high_sample_shift_packet, 1, oversampl);
+
 rx_shift = rx((rrcos_filt_len + 1):(end - rrcos_filt_len));
+rx_without_header = rx_shift(length(packet_header)+1:end);
+rx_header = rx_shift(1:length(barker_symbs));
+phase_error = mean(angle(rx_header.*barker_symbs));
 
-scale_factor = 0.9;
-symbs_rx = rx_shift/max(abs(rx_shift))/scale_factor*sqrt(2);
+rx_phase_corrected = rx_without_header*exp(i*(pi/2-phase_error));
+gain_comp = mean(abs(barker_symbs./rx_header));
+
+symbs_rx = rx_phase_corrected * gain_comp;
 
 figure
 hold on
@@ -70,7 +71,9 @@ axis([-1.5 1.5 -1.5 1.5])
 
 rx_bin_symbs = zeros(size(symbs));
 for k = 1:length(symbs_rx)
-   [dont_care, qam_i] = min(abs(symbs_rx(k)-qam16_lookup));
+   [dont_care, qam_i] = min(abs(symbs_rx(k)-qam_lookup));
+   resolved_symb = qam_lookup(qam_i);
+   
    rx_bin_symbs(k) = qam_i - 1;
 end
 
--- transmitter.m
+++ transmitter.m
@@ -1,15 +1,12 @@
-function [txdata, t_up] = transmitter(symbs, oversampl, flo, rrcos_filt)
+function [txdata, t_up] = transmitter(symbs, oversampl, flo, rrcos_filt, fs)
 %TRANSMITTER Summary of this function goes here
 %   Detailed explanation goes here
     symbs_pulse_shaped = upfirdn(symbs, rrcos_filt, oversampl, 1);
-    symbs_pulse_shaped_up = resample(symbs_pulse_shaped, flo, 1);
-    
-    t_up = linspace(1, length(symbs), length(symbs_pulse_shaped_up));
-    lo_in = sin(2*pi*flo*t_up);
-    lo_out = cos(2*pi*flo*t_up);
-    hold on
-    %plot(linspace(1, length(symbs), length(symbs_pulse_shaped)), real(symbs_pulse_shaped))
-    plot(t_up, lo_in);
+    symbs_pulse_shaped_up = resample(symbs_pulse_shaped, fs*flo, 1);
+    t_up = 0:1/fs:((length(symbs_pulse_shaped_up)-1)/fs);
+    lo_in = sin(t_up);
+    lo_out = cos(t_up);
+
     txdata = real(symbs_pulse_shaped_up).*lo_in + imag(symbs_pulse_shaped_up).*lo_out;
 end
 
